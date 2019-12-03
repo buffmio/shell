b56b1bc67877a37e40a6158d03d9e454f314137d
oswaldlau <39292816+oswaldlau@users.noreply.github.com>
2019-12-03 11:25:43 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo 'A1234567@b'| passwd --stdin root
+
+yum install git -y
+
+git clone https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+
+cat > /etc/conf.d/wondershaper.conf <<EOF
+[wondershaper]
+# Adapter
+#
+IFACE="eth0"
+
+# Download rate in Kbps
+#
+DSPEED="102400"
+
+# Upload rate in Kbps
+#
+USPEED="3072"
+EOF
+
+systemctl daemon-reload
+systemctl enable wondershaper
+systemctl restart wondershaper
+
+rm -rf /root/wondershaper.sh*
+rm -rf /root/wondershaper
