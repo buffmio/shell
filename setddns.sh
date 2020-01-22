c8b74b9113f1b8a2220494de9563501c4cdaf06a
oswaldlau <39292816+oswaldlau@users.noreply.github.com>
2020-01-22 20:14:41 +0800

    Add files via upload

---
+++ setddns.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+wget -qO /usr/local/bin/ddns.sh https://raw.githubusercontent.com/oswaldlau/shell/master/ddns.sh
+read -p "动态域名:" inputDomain
+read -p "公云用户名:" inputUser
+read -p "公云密码:" inputPassword
+sed -i '2s/^.*$/Domain='$inputDomain'/' /usr/local/bin/ddns.sh
+sed -i '3s/^.*$/User='$inputUser'/' /usr/local/bin/ddns.sh
+sed -i '4s/^.*$/Password='$inputPassword'/' /usr/local/bin/ddns.sh
+
+#配置定时任务
+
+cat >/lib/systemd/system/ddns.service <<EOF
+[Unit]
+Description=设置ddns
+After=network-online.target
+Wants=network-online.target
+[Service]
+WorkingDirectory=/root/
+EnvironmentFile=
+ExecStart=/bin/bash /usr/local/bin/ddns.sh
+Restart=always
+RestartSec=30
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable ddns >/dev/null 2>&1
+service ddns stop >/dev/null 2>&1
+service ddns start >/dev/null 2>&1
