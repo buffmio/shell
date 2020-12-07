f5da56301f48431abbf5b38e8abd795e0116d3d3
buffmio <39292816+buffmio@users.noreply.github.com>
2020-12-07 15:37:10 +0800

    Update initos.sh

--- initos.sh
+++ initos.sh
@@ -4,11 +4,11 @@ export PATH
 
 echo 'A1234567b@2020'| passwd --stdin root
 
-yum install git -y
+yum install wget -y
 
-cd /opt
+wget -P /opt https://raw.githubusercontent.com/buffmio/LightsailLimitTraffic/master/500G.py
 
-
+cat > /etc/systemd/system/traffic.service <<EOF
 [Unit]
 Description=traffic
 After=rc-local.service
@@ -18,8 +18,12 @@ Type=simple
 User=root
 Group=root
 WorkingDirectory=/opt
-ExecStart=/usr/bin/python 1000G.py
+ExecStart=/usr/bin/python 500G.py
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
+
+EOF
+
+systemctl start traffic.service 
