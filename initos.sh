c85ab1529c3651a2372c2cfe453b28655b899379
buffmio <39292816+buffmio@users.noreply.github.com>
2020-12-07 15:26:50 +0800

    Update initos.sh

--- initos.sh
+++ initos.sh
@@ -1,15 +1,24 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo 'A1234567b@2020'| passwd --stdin root
+
+yum install git -y
+
+cd /opt
 
 
 [Unit]
-Description=test deamon
+Description=traffic
 After=rc-local.service
 
 [Service]
 Type=simple
 User=root
 Group=root
-WorkingDirectory=/home
-ExecStart=/usr/bin/python test.py
+WorkingDirectory=/opt
+ExecStart=/usr/bin/python 1000G.py
 Restart=always
 
 [Install]
